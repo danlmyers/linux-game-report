bbee6a4f95330f4caacc22e01625e2fc76463a6c
Daniel Myers <dan@moird.com>
2015-03-29 14:14:09 -0700

    Added Debug Logging

--- steamcheck/__init__.py
+++ steamcheck/__init__.py
@@ -2,8 +2,13 @@ __author__ = 'Daniel Myers'
 import logging
 
 logging.getLogger("requests").setLevel(logging.WARNING)
+from logging import StreamHandler
 
 from flask import Flask
 app = Flask(__name__)
 
 import steamcheck.views
+
+file_handler = StreamHandler()
+app.logger.setLevel(logging.DEBUG)
+app.logger.addHandler(file_handler)
